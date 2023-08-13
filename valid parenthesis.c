bb57926858b699b982700ee014ad5dc4eaef66a9
Sairaj Shirole <121667877+sairaj2003@users.noreply.github.com>
2023-08-13 11:11:43 +0530

    Update valid parenthesis.c

--- valid parenthesis.c
+++ valid parenthesis.c
@@ -1,40 +1,75 @@
 /*
-valid parenthesis
+DAY 12
+
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false
 */
+
 #include <stdio.h>
-#include<string.h>
-int validParentheses(int n,char word[])
+#include <string.h>
+int validString(int n,char string[])
 {
-    int openCount=0;
-    int closeCount=0;
+    int stack[100];
+    int top=-1;
     
     for(int i=0;i<n;i++)
     {
-        if(word[i]== '(' || word[i]=='{' || word[i]=='[')
+        if(string[i] == '(' || string[i] == '[' || string[i] == '{')
         {
-            openCount++;
+            stack[++top]=string[i];
         }
-        else
+        else if(string[i] == ')' || string[i] == ']' || string[i] == '}')
         {
-            closeCount++;
+            if(top==-1)
+            {
+                return 0;
+            }
+            
+            char opening=stack[top--];
+            if((opening== '(' && string[i] != ')') || 
+            (opening == '[' && string[i] != ']') ||
+            (opening == '{' && string[i] != '}'))
+            {
+                return 0;
+            }
         }
     }
+    return (top == -1); 
+}
+int main() {
+    char string[100];
+    printf("\nEnter string of symbols:");
+    scanf("%s",string);
     
-    if(openCount == closeCount)
+    int n=strlen(string);
+    int result=validString(n,string);
+    
+    if(result)
     {
-        printf("\nIt is valid parenthese...");
+        printf("\nValid string...");
     }
     else
     {
-        printf("\nNot valid parenthese...");
+        printf("\nNot valid string..");
     }
-}
-int main() {
-    char word[100];
-    printf("\nEnter String:");
-    scanf("%s",word);
-    
-    int n=strlen(word);
-    validParentheses(n,word);
     return 0;
 }
